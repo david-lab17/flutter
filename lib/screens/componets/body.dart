9283d22bd1b683ec522cbbfb41147579df391271
david-lab17 <davidmakuba@yahoo.com>
2021-04-26 21:28:10 +0300

    flutter online

---
+++ lib/screens/componets/body.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/constants.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories(),
+      ],
+    );
+  }
+}
+
+//We need stateful widget to our categories
+class Categories extends StatefulWidget {
+  Categories({Key key}) : super(key: key);
+
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ["hand bag", "Jewellery", "Footwear", "Dresses"];
+  //by default 1st item will be selected
+  int selectedindex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (context, index) => Text(
+                categories[index],
+              )),
+    );
+  }
+}
